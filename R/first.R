4f47e36ed1675bb785be777554ed2c27213c950a
Dave Lorenz <lorenz@usgs.gov>
2013-11-05 14:14:02 -0600

    removed unnecessary .First.lib function

--- R/first.R
+++
@@ -1,9 +0,0 @@
-# Function to prevent users from modifying any data in this package.
-# Coding history:
-#    2011Jul14 DLLorenz Original Coding
-#    2011Jul14          This version
-#
-
-.First.lib <- function(...) {
-    lockEnvironment(as.environment("package:USGSdata"), TRUE)
-}
